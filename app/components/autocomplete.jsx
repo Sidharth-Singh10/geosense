48a3678e088422562ed2b5cd9c9c3f8e2abf7d94
Maneesh Shukla <shuklamaneesh24@gmail.com>
2025-02-15 17:22:26 +0530

    navbar styled.

--- app/components/autocomplete.jsx
+++ app/components/autocomplete.jsx
@@ -40,7 +40,7 @@ const PlaceAutocomplete = () => {
   return (
     <input
       ref={inputRef}
-      className="w-full px-4 py-2 text-gray-700 border rounded-lg focus:outline-none focus:border-blue-500"
+      className="w-full px-4 py-2 text-white border font-mono bg-white/5 rounded-lg focus:outline-none focus:border-blue-500"
       placeholder="Search for a place..."
     />
   );
--- app/components/navbar.jsx
+++ app/components/navbar.jsx
@@ -9,18 +9,15 @@ export default function Navbar() {
   const { overlayOn, setOverlayOn } = useUserContext();
 
   return (
-    // <nav className="bg-gray-500 flex items-center justify-between px-6 py-3 shadow-md rounded-full h-16 mx-auto w-[90%]">
     <nav className="bg-white/10 backdrop-blur-lg border border-white/20 shadow-lg shadow-black/20 flex items-center justify-between px-6 py-3 rounded-full h-16 mx-auto w-[90%]">
  
     {/* Left Section - Search Bar */}
-      {/* <div className="flex items-center bg-gray-700 px-4 py-2 rounded-full w-1/3 mr-8"> */}
-      <div className="flex items-center bg-white/10 backdrop-blur-lg border border-white/20 shadow-lg shadow-black/20 px-4 py-2 rounded-full w-1/3 mr-8">
+      <div className="flex items-center px-4 py-2 rounded-full w-1/3 mr-8">
 
-        {/* <Search className="text-gray-500 w-5 h-5 mr-2" />
-         */}
-         <Search className="text-white/70 w-5 h-5 mr-2 drop-shadow-sm" />
+         <Search className="text-white/70  w-5 h-5 mr-2 drop-shadow-sm" />
 
         <PlaceAutocomplete />
+        
       </div>
 
       {/* Center Section - Navigation Icons */}
