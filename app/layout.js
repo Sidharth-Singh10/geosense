29451768c2b44dba5cbb77d4cfc63df208721557
Maneesh Shukla <shuklamaneesh24@gmail.com>
2025-02-15 17:26:07 +0530

    toast

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { UserProvider } from "./components/user_context";
 import { APIProvider } from "@vis.gl/react-google-maps";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +24,7 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <UserProvider>
+        <ToastContainer />
           <APIProvider
             apiKey={"AIzaSyCBUWqISO_DOQUKhwb7q09wQteK87WOEec"}
             libraries={["places"]}
